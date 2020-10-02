31b3362a0b97ed433ec016ccc977e253c8274fb7
jhcarrington <jhcarrington@birdwellsolutions.com>
2020-10-02 13:04:33 -0500

    Extra Credit- Hive image

--- spike-ui/src/src/components/home/AddHive/AddHive.jsx
+++ spike-ui/src/src/components/home/AddHive/AddHive.jsx
@@ -1,18 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import { Typography, Button, Box, TextField, Card, Dialog, Slider, FormControl, InputLabel, Select, MenuItem, Checkbox, FormControlLabel } from "@material-ui/core";
 import { Hive } from '../../../libs/util.ts';
 import './AddHive.css';
 import EquipmentSelect from "./EquipmentSelect";
-import {InventoryEquipmentTypes} from "../../../constants/Hive.js"
-import {HiveEquipmentTypes} from "../../../constants/Hive.js"
+import { InventoryEquipmentTypes } from "../../../constants/Hive.js"
+import { HiveEquipmentTypes } from "../../../constants/Hive.js"
 
 
 
 const AddHive = React.forwardRef((props, ref) => {
 
-    
+
     const [values, setValues] = useState({
         name: '',
         inspectionResults: '',
@@ -23,12 +23,28 @@ const AddHive = React.forwardRef((props, ref) => {
         inventoryEquipment: [],
         losses: '',
         gains: '',
-        viewable: false
-      });
+        viewable: false,
+        image: undefined
+    });
 
+    const handleFile = (event) => {
+        const { target } = event;
+        const { files } = target;
+
+        if (files && files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = event => {
+
+                setValues({ ...values, image: event.target?.result });
+            };
+
+            reader.readAsDataURL(files[0]);
+        }
+    }
     const handleChange = (prop) => (event, value) => {
         let newValue;
-        if(event.target.value) {
+        if (event.target.value) {
             newValue = event.target.value
         } else {
             newValue = value
@@ -49,28 +65,35 @@ const AddHive = React.forwardRef((props, ref) => {
             losses: values.losses,
             gains: values.gains,
             viewable: values.viewable,
-            hiveId: null
+            hiveId: null,
+            image: values.image
         }
-        let response = await Hive.create(localStorage.getItem('auth'), hive);
-        hive.hiveId = response.data.hiveId;
-        props.handleClose(hive);
-    } 
+        console.log("JASON", values.image, '\nHELLO', hive.image)
+        try {
+            let response = await Hive.create(localStorage.getItem('auth'), hive)
+            hive.hiveId = response.data.hiveId;
+            props.handleClose(hive);
+        }
+        catch (error) {
+            alert("Image too large, upload an image less than 400KB");
+        }
+    }
     return (
         <Dialog className="add-modal" open={props.open} onClose={props.handleClose}>
             <Card className="overflow-card">
                 <AppBar position="static" className="add-bar" color="secondary">
-                    <Typography variant="h6" style={{flexGrow: 1}}>
+                    <Typography variant="h6" style={{ flexGrow: 1 }}>
                         Add a new Hive
                     </Typography>
                 </AppBar>
                 <Grid container className="add">
                     <form noValidate id="add-form" onSubmit={handleSubmit} autoComplete="off">
-                        <TextField className="modal-textbox-full name-field" 
-                                            id="name" 
-                                            value={values.name} 
-                                            onInput={handleChange("name")} 
-                                            label="Name" 
-                                            variant="outlined" />
+                        <TextField className="modal-textbox-full name-field"
+                            id="name"
+                            value={values.name}
+                            onInput={handleChange("name")}
+                            label="Name"
+                            variant="outlined" />
                         <FormControl variant="outlined" className="modal-select health" >
                             <InputLabel >Health Level</InputLabel>
                             <Select
@@ -79,44 +102,44 @@ const AddHive = React.forwardRef((props, ref) => {
                                 id="health-select"
                                 value={values.health}
                                 onChange={handleChange('health')}
-                                >
-                                {[0,1,2,3,4,5,6,7,8,9,10].map((hp) => (
+                            >
+                                {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((hp) => (
                                     <MenuItem key={hp} value={hp}>
                                         {hp}
                                     </MenuItem>
                                 ))}
                             </Select>
                         </FormControl>
-                        <FormControl  className="slider" variant="outlined">
+                        <FormControl className="slider" variant="outlined">
                             <InputLabel >Honey</InputLabel>
-                            <Slider valueLabelDisplay="auto" value={values.honeyStores} onChange={handleChange('honeyStores')}/>
+                            <Slider valueLabelDisplay="auto" value={values.honeyStores} onChange={handleChange('honeyStores')} />
                         </FormControl>
-                        
-                        <TextField className="modal-textbox-full" 
-                                            id="username" 
-                                            value={values.queenProduction} 
-                                            onInput={handleChange("queenProduction")} 
-                                            label="Queen Production" 
-                                            variant="outlined" />
-                        <TextField className="modal-textbox-half" 
-                                            id="losses" 
-                                            value={values.losses} 
-                                            onInput={handleChange("losses")} 
-                                            label="Losses" 
-                                            variant="outlined" />
-                        <TextField className="modal-textbox-half" 
-                                            id="gains" 
-                                            value={values.gains} 
-                                            onInput={handleChange("gains")} 
-                                            label="Gains" 
-                                            variant="outlined" />
+
+                        <TextField className="modal-textbox-full"
+                            id="username"
+                            value={values.queenProduction}
+                            onInput={handleChange("queenProduction")}
+                            label="Queen Production"
+                            variant="outlined" />
+                        <TextField className="modal-textbox-half"
+                            id="losses"
+                            value={values.losses}
+                            onInput={handleChange("losses")}
+                            label="Losses"
+                            variant="outlined" />
+                        <TextField className="modal-textbox-half"
+                            id="gains"
+                            value={values.gains}
+                            onInput={handleChange("gains")}
+                            label="Gains"
+                            variant="outlined" />
                         <FormControl variant="outlined" className="modal-select" >
                             <InputLabel >Inventory Equipment</InputLabel>
-                            <EquipmentSelect equipment={values.inventoryEquipment} equipmentChoices={InventoryEquipmentTypes} type="inventoryEquipment" handleChange={handleChange}/>
+                            <EquipmentSelect equipment={values.inventoryEquipment} equipmentChoices={InventoryEquipmentTypes} type="inventoryEquipment" handleChange={handleChange} />
                         </FormControl>
                         <FormControl variant="outlined" className="modal-select" >
                             <InputLabel >Hive Equipment</InputLabel>
-                            <EquipmentSelect equipment={values.hiveEquipment} equipmentChoices={HiveEquipmentTypes} type="hiveEquipment" handleChange={handleChange}/>
+                            <EquipmentSelect equipment={values.hiveEquipment} equipmentChoices={HiveEquipmentTypes} type="hiveEquipment" handleChange={handleChange} />
                         </FormControl>
                         <FormControl variant="outlined" className="modal-textbox-half" >
                             <InputLabel >Inspection Result</InputLabel>
@@ -125,25 +148,45 @@ const AddHive = React.forwardRef((props, ref) => {
                                 variant="outlined"
                                 value={values.inspectionResults}
                                 onChange={handleChange('inspectionResults')}
-                                >
-                                    <MenuItem value="Pass">
-                                        Pass
+                            >
+                                <MenuItem value="Pass">
+                                    Pass
                                     </MenuItem>
-                                    <MenuItem value="Fail" className="danger">
-                                        Fail
+                                <MenuItem value="Fail" className="danger">
+                                    Fail
                                     </MenuItem>
                             </Select>
                         </FormControl>
                         <FormControl required>
-                            <FormControlLabel 
+                            <FormControlLabel
                                 control={<Checkbox onChange={handleChange('viewable')} />} label="Viewable to Public"
                                 labelPlacement="start"
                                 className="viewable-checkbox"
                             />
                         </FormControl>
+                        <FormControl style={{  alignItems: 'center', marginTop: 10}}>
+                            <img style={{
+                                height: '10vw',
+                                width: '10vw',
+                                borderRadius: 25,
+                                border: '1px solid black',
+                                objectFit: 'cover',
+                                objectPosition: 'center',
+                                borderWidth: 1
+                            }} src={values.image} alt={`Hive Picture
+                            (optional)`} >
+                            </img>
+                            <input
+                                id="car"
+                                type="file"
+                                accept="image/*"
+                                capture="camera"
+                                onChange={handleFile}
+                            />
+                        </FormControl>
                         <Box className="submit-add">
                             <Button className="submit-button" type="submit" form="add-form" variant="contained" color="primary">
-                            Submit
+                                Submit
                             </Button>
                         </Box>
                     </form>
