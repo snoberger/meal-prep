5d782dad4a26a46a024c3aabcbd7a51826489115
jhcarrington <jhcarrington@birdwellsolutions.com>
2020-10-02 16:48:17 -0500

    Auto Refresh hives when one is changed

--- spike-ui/src/src/components/home/Home.jsx
+++ spike-ui/src/src/components/home/Home.jsx
@@ -1,17 +1,17 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
-import {Grid, Accordion, IconButton, AccordionActions, AccordionSummary} from '@material-ui/core';
+import { Grid, Accordion, IconButton, AccordionActions, AccordionSummary } from '@material-ui/core';
 import Create from '@material-ui/icons/Create';
 import { Typography } from "@material-ui/core";
 import Hive from "./Hive";
 import AddHive from "./AddHive/AddHive";
 import DeleteHive from "./DeleteHive/DeleteHive";
-import {getUsersHives} from "../../libs/Hive";
+import { getUsersHives } from "../../libs/Hive";
 import { Add } from "@material-ui/icons";
 import EditHive from "./EditHive/EditHive"
 
 export default function Home() {
-    
+
     const [hives, setHives] = useState([]);
     const [open, setOpen] = useState(false);
     const [currentHive, setCurrentHive] = useState();
@@ -21,7 +21,7 @@ export default function Home() {
         setOpen(true);
     };
 
-    const editClick = (hive)=>() => {
+    const editClick = (hive) => () => {
         setCurrentHive(hive)
         console.log("hello", hive, currentHive)
     }
@@ -31,13 +31,13 @@ export default function Home() {
                 <Accordion style={{ backgroundColor: "#dcdcdc" }}>
                     <AccordionSummary>
                         <Typography variant="h5">{hive.name}</Typography>
-                    </AccordionSummary>                    
+                    </AccordionSummary>
                     <AccordionActions>
                         <IconButton color="primary" size="small" component="span" onClick={editClick(hive)}>
-                            <Create/>
-                            
+                            <Create />
+
                         </IconButton>
-                        <DeleteHive hive={hive} setHives={setHives} allHives={hives}/>
+                        <DeleteHive hive={hive} setHives={setHives} allHives={hives} />
                     </AccordionActions>
                     <Hive hiveData={hive}></Hive>
                 </Accordion>
@@ -46,7 +46,7 @@ export default function Home() {
     }
 
     const handleClose = (hive) => {
-        if(hive.name){
+        if (hive.name) {
             let temp = hives.slice();
             temp.push(hive);
             setHives(temp)
@@ -69,24 +69,34 @@ export default function Home() {
             hasLoaded.current = true;
         });
     }, [hasLoaded]);
-    
+
     return (
         <div>
             <NavBar />
-            <Grid style={{ marginTop: "5em"}} container justify="center" >
+            <Grid style={{ marginTop: "5em" }} container justify="center" >
                 <Grid>
                     {hives.length ? setupViewHives(hives) : <span>No hives found.</span>}
                 </Grid >
-                <Grid style={{width: '100%'}}>
+                <Grid style={{ width: '100%' }}>
                     <IconButton color="primary" onClick={handleOpen} component="span">
                         <Add />
                     </IconButton>
                 </Grid >
             </Grid>
-            <AddHive className="add-modal" open={open} handleOpen={handleOpen} handleClose={handleClose}/>
-            <EditHive hive={currentHive} handleClose={()=>{
+            <AddHive className="add-modal" open={open} handleOpen={handleOpen} handleClose={handleClose} />
+            <EditHive hive={currentHive} handleClose={() => {
                 setCurrentHive(undefined)
-            }} /> 
+
+                const loadHives = async () => {
+                    let userId = localStorage.getItem('auth');
+                    let userHives = (await getUsersHives(userId)).data.Items;
+                    return userHives;
+                }
+                loadHives().then(d => {
+                    setHives(d);
+                    hasLoaded.current = true;
+                });
+            }} />
         </div>
     )
 }
