b76149e47c3f6cb793238026afc5790fa03ecab6
jhcarrington <jhcarrington@birdwellsolutions.com>
2020-10-22 17:50:50 -0500

    Get coverage to 100%

--- mp-ui/jest.config.js
+++ mp-ui/jest.config.js
@@ -1,13 +1,12 @@
-const esModules = ['@agm', 'ngx-bootstrap', 'lodash-es'].join('|');
-
 module.exports = {
-  collectCoverageFrom: ["tests/**/*.{ts,tsx,mjs}"],
-  testMatch: ["<rootDir>/tests/**/*.spec.{ts,tsx,mjs}"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx,mjs}"],
+  testMatch: ["<rootDir>/tests/**/*.spec.{ts,tsx,mjs}" ],
   transform: {
     '^.+\\.(tsx|jsx|ts|js|html)$': 'ts-jest',
     ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
   },
   setupFilesAfterEnv: [
     "<rootDir>/tests/setupTests.ts"
-  ]
+  ],
+  coveragePathIgnorePatterns: ['<rootDir>/src/serviceWorker.ts', '<rootDir>/src/index.tsx', '<rootDir>/src/react-app-env.d.ts']
 };
