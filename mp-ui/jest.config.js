398b3c2f1a1ab6134026e4de55c09ffbb6dd0c1c
snoberger <snoberger@wisc.edu>
2020-10-27 20:51:27 -0500

    reverting / to \ change in jest.config

--- mp-ui/jest.config.js
+++ mp-ui/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  collectCoverageFrom: ["src\\**\\*.{ts,tsx,mjs}"],
-  testMatch: ["<rootDir>\\tests\\**\\*.spec.{ts,tsx,mjs,js,jsx}" ],
+  collectCoverageFrom: ["src/**/*.{ts,tsx,mjs}"],
+  testMatch: ["<rootDir>/tests/**/*.spec.{ts,tsx,mjs,js,jsx}" ],
   transform: {
     '^.+\\.(tsx|jsx|ts|js|html)$': 'ts-jest',
     ".+\\.(css|styl|less|sass|scss)$": "jest-transform-css"
   },
   setupFilesAfterEnv: [
-    "<rootDir>\\tests\\setupTests.ts"
+    "<rootDir>/tests/setupTests.ts"
   ],
-  coveragePathIgnorePatterns: ['<rootDir>\\src\\serviceWorker.ts', '<rootDir>\\src\\index.tsx', '<rootDir>\\src\\react-app-env.d.ts'],
+  coveragePathIgnorePatterns: ['<rootDir>/src/serviceWorker.ts', '<rootDir>/src/index.tsx', '<rootDir>/src/react-app-env.d.ts'],
 };
