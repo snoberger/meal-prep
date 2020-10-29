ec989bf7f3db6cbbc7144e8934c4f0d83cecc221
snoberger <snoberger@wisc.edu>
2020-10-29 18:56:58 -0500

    added tests and removed authorizer for public endpoints

--- mp-backend/src/middleware/authenticateJWT.ts
+++ mp-backend/src/middleware/authenticateJWT.ts
@@ -2,12 +2,19 @@
 
 import {Callback, Context } from 'aws-lambda'
 import { APIGatewayAuthorizerResult, PolicyDocument } from 'aws-lambda/trigger/api-gateway-authorizer';
-import { APIGatewayTokenAuthorizerEvent, Statement } from 'aws-lambda/trigger/api-gateway-authorizer'
+import { APIGatewayTokenAuthorizerEvent } from 'aws-lambda/trigger/api-gateway-authorizer'
 import {authLib} from '../libs/authentication'
 
 type Policy = {
     principalId: string,
     policyDocument: PolicyDocument
+}
+
+type JWTResponse = {
+    userId: string,
+    iat?: number, 
+    exp?: number, 
+    iss?: string
 }
 const generatePolicy = (principalId: string, effect: string, resource: string): Policy => {
     const policyDocument: PolicyDocument = {
@@ -28,11 +35,24 @@ const generatePolicy = (principalId: string, effect: string, resource: string): 
 };
 
 
-export const authenticateJWT = (event: APIGatewayTokenAuthorizerEvent, _context: Context, callback: Callback<APIGatewayAuthorizerResult>) => {
-    console.log(event)
-    // if(!event.authorizationToken){
-        // callback('Unauthorized')
-    // }
-    console.log(event)
-    return callback(null, generatePolicy('1', 'Allow', event.methodArn))
+export const authenticateJWT = (event: APIGatewayTokenAuthorizerEvent, _context: Context, callback: Callback<APIGatewayAuthorizerResult>): void => {
+    if (!event.authorizationToken) {
+        return callback('Unauthorized');
+    }
+    
+    const tokenParts = event.authorizationToken.split(' ');
+    const tokenValue = tokenParts[1];
+    if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
+        return callback('Unauthorized');
+    }
+    try {
+        //throws error if invalid but making sure decoded data is expected format
+        const result: JWTResponse = (<JWTResponse>authLib.verifyJWT(tokenValue))
+        if(result && result.userId){
+            return callback(null, generatePolicy(result.userId, 'Allow', event.methodArn))
+        }
+    } catch (e) {
+        return callback('Unauthorized');
+    }
+    return callback('Unauthorized');
 }
