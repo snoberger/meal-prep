723fa5645c16a589d8402d008cde14d17c4ce58a
Caleb Floyd <ctfloyd@wisc.edu>
2020-12-01 17:45:21 -0600

    revert changes to url

--- mp-ui/src/api/const.ts
+++ mp-ui/src/api/const.ts
@@ -1,1 +1,1 @@
-export const ENDPOINT: string = process.env.NODE_ENV === 'production' ? 'https://4vt0v9rlpj.execute-api.us-east-1.amazonaws.com/dev/api' : 'http://localhost:3001/api';
+export const ENDPOINT: string = 'http://localhost:3001/api';
