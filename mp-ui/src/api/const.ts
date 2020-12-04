65287e05f144f8ed07b431c3cedd0d9cc9fc43f8
snoberger <snoberger@wisc.edu>
2020-12-04 09:57:42 -0600

    add api to url

--- mp-ui/src/api/const.ts
+++ mp-ui/src/api/const.ts
@@ -1,7 +1,7 @@
 export const ENDPOINT = function() {
     switch(process.env.stage) {
         case 'prod':
-            return 'https://wdbb06ys49.execute-api.us-east-1.amazonaws.com/dev';
+            return 'https://wdbb06ys49.execute-api.us-east-1.amazonaws.com/dev/api';
         default:
             return 'http://localhost:3001/api';
     }
